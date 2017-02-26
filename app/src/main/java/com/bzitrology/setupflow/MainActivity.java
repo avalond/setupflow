8a9ca7b58fcad5013c5a23c454f42504992bce2f
kevin <agonyice0115@gmail.com>
2017-02-26 17:26:42 +0800

    update

--- app/src/main/java/com/bzitrology/setupflow/MainActivity.java
+++ app/src/main/java/com/bzitrology/setupflow/MainActivity.java
@@ -1,25 +1,36 @@
 package com.bzitrology.setupflow;
 
+import com.bzitrology.setupflow.adapter.MyFragmentPagerAdapter;
+
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-  private TabLayout mTabLayout;
+	private TabLayout mTabLayout;
+	private ViewPager mViewPager;
+	private MyFragmentPagerAdapter mPagerAdapter;
+	private Context mContext;
 
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    mTabLayout = (TabLayout) findViewById(R.id.tablayout);
-    mTabLayout.addTab(mTabLayout.newTab().setText("Tab 4"));
+	@Override
 
-    mTabLayout.addTab(mTabLayout.newTab().setText("Tab 5"));
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mContext = MainActivity.this;
+		mTabLayout = (TabLayout) findViewById(R.id.tablayout);
+		mViewPager = (ViewPager) findViewById(R.id.viewPager);
 
-    mTabLayout.addTab(mTabLayout.newTab().setText("Tab 6"));
+		FragmentManager manager = getSupportFragmentManager();
+		mPagerAdapter = new MyFragmentPagerAdapter(manager, mContext);
+		mViewPager.setAdapter(mPagerAdapter);
+		mTabLayout.setTabTextColors(Color.GRAY, Color.BLACK);
 
-    mTabLayout.addTab(mTabLayout.newTab().setText("Tab 7"));
-
-  }
+		mTabLayout.setupWithViewPager(mViewPager);
+	}
 }
