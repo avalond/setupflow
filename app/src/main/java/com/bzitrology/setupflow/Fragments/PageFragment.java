a678eef6119ea32cf361761919f358ce32bc33ed
kevin <agonyice0115@gmail.com>
2017-02-28 23:31:20 +0800

    update code

--- app/src/main/java/com/bzitrology/setupflow/Fragments/PageFragment.java
+++ app/src/main/java/com/bzitrology/setupflow/Fragments/PageFragment.java
@@ -8,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * @author by kevin.
@@ -31,8 +30,6 @@ public class PageFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_page, container, false);
-		TextView textView = (TextView) view.findViewById(R.id.textView);
-		textView.setText("restaurant page");
 		return view;
 	}
 }
